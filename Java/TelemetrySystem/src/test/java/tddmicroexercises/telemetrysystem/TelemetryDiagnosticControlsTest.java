6dc19a92dec922532c19596f8c502ffd0ff608b9
AbhilashZemoso <abhilash.notla@zemosolabs.com>
2023-01-24 17:30:33 +0530

    applied SOLID principles

--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -1,13 +1,14 @@
 package tddmicroexercises.telemetrysystem;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-public class TelemetryDiagnosticControlsTest
-{
-    
-	@Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
-    {
+public class TelemetryDiagnosticControlsTest {
+
+    @Test
+    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+        TelemetryDiagnosticControls controls = new TelemetryDiagnosticControls();
+        controls.checkTransmission();
+        Assertions.assertFalse(controls.getDiagnosticInfo().isEmpty());
     }
-
 }
